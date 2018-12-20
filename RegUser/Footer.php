f3441d6d933b915660ee6eec84037a7ad5f450b6
mostafasaroarovi1 <datavault56@gmail.com>
2018-12-20 20:47:50 +0600

    Added footer file in reguser

---
+++ RegUser/Footer.php
@@ -0,0 +1,12 @@
+﻿<div id="templatemo_content_bottom"></div>
+
+
+<div id="templatemo_footer">
+    <ul class="footer_menu">
+        <li><a href="Index.php" class="current">Home</a></li>
+        
+        
+    </ul>
+
+    Copyright © 2018 Crime Management System</div> 
+<!-- end of footer -->
