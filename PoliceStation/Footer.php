1c6396d3f7a46a3248d916f7ce42a9fb86b0a24c
abdullahalnoman1520 <abdullah.sarker.cse@ulab.edu.bd>
2018-12-26 13:06:38 +0600

    Footer file to police Station

---
+++ PoliceStation/Footer.php
@@ -0,0 +1,12 @@
+<div id="templatemo_content_bottom"></div>
+
+
+<div id="templatemo_footer">
+    <ul class="footer_menu">
+        <li><a href="Index.php" class="current">Home</a></li>
+        <li><a href="Profile.php">Profile</a></li>
+        <li><a href="Logout.php">Logout</a></li>
+    </ul>
+
+    Copyright © 2018 Crime Management System</div> 
+<!-- end of footer -->
